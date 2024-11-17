e4c0040efbf234e25753e1e3b99647216ec184c4
Süleyman <sduymaz8@gmail.com>
2024-11-17 16:50:30 +0300

    microsoft cache base

--- Final/Business/obj/Debug/net5.0/Business.AssemblyInfo.cs
+++ Final/Business/obj/Debug/net5.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7241a8fbb5e8eb802160dd3818da0af7950778cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c39946637a8d41341084cb96b0c59afad19ca02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Final/Core/DependencyResolvers/CoreModule.cs
+++ Final/Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 namespace Core.DependencyResolvers
@@ -7,7 +9,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            //caching mekanizmasının çalışması için.
+            serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheService, MemoryCacheManager>();
         }
     }
 }
