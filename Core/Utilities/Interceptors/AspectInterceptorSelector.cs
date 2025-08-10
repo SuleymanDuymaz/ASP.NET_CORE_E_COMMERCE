4c34c07b49375455c6617314bb9fd414f369e006
Süleyman <sduymaz8@gmail.com>
2025-08-10 17:44:05 +0300

    logging
    logging

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,27 @@
+﻿using Castle.DynamicProxy;
+using Core.Aspect.Autofac.Exception;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+            classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
